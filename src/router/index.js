eee503b5fc2bc7324dfcf08cb67c03750d0a2a80
WangXiaoXi <995107408@qq.com>
2018-04-10 23:07:26 +0800

    添加登录验证
    添加登录验证

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from 'site/home/home'
+import store from '../store/index'
 import Uindex from 'ucenter/uindex/uindex'
 import Login from 'login/login'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
     {
       path: '/',
@@ -29,3 +30,25 @@ export default new Router({
     }
   ]
 })
+
+// 页面刷新时，重新赋值token
+// if (window.localStorage.getItem('token')) {
+//   store.commit(types.LOGIN, window.localStorage.getItem('token'))
+// }
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(r => r.meta.requireAuth)) {
+    if (store.state.token) {
+      next()
+    } else {
+      next({
+        path: '/login',
+        query: {redirect: to.fullPath}
+      })
+    }
+  } else {
+    next()
+  }
+})
+
+export default router
